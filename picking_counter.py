f872ea60ae9101231b7329742b7d6d32acfa822d
SlothArms <nickbiacsi@gmail.com>
2024-11-24 20:30:28 -0500

    Changed file structure to Linux

--- picking_counter.py
+++ picking_counter.py
@@ -151,7 +151,7 @@ class GUI:
 
 
 def main() -> None:
-    file_path: str = r"D:\Misc\Scripts\Picking Counter\Log.csv"
+    file_path: str = "/home/sloth/Documents/Scripts/Picking_Counter/personal-game-picking-counter/Log.csv"
     GUI(file_path)
 
 
